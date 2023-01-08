7f5feadc604ff7f52e5710226c1b21c55bcd14d4
ëmanuelsuarez¨ <braianesuarez@gmail.com>
2023-01-08 14:42:53 -0300

    [new] Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,20 @@ import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
-
+ 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding >
+    <LeftSection>
+      <SectionTitle main center >
+        Welcome To <br/>
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Hi, I'm Emanuel Suarez, a full-stack web developer based in Buenos Aires. I enjoy to develop websites for both individuals and companies experimenting with all kinds of technologies to convey stories.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://linkedin.com/in/emanuelsuarez'} >Get in Touch</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
