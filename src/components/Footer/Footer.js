ddbfa4931939cfb0773e55b2a73a10c4851f652f
ëmanuelsuarez¨ <braianesuarez@gmail.com>
2023-01-08 16:38:26 -0300

    [new] Footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,32 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:+541139088010' >+54 9 11 3908 8010</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:braianesuarez@gmail.com' >braianesuarez@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>Innovating one project at a time</CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href='https://github.com/EmanuelSuarez' target="_blank" >
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href='https://linkedin.com/in/emanuelsuarez' target="_blank" >
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href='https://instagram.com/braiannsuarez  ' target="_blank" >
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
