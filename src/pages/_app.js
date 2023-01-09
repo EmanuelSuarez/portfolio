ead2027c6833dbb566a636fb1d0634b5b673a9d7
ëmanuelsuarez¨ <braianesuarez@gmail.com>
2023-01-09 01:05:33 -0300

    [update] metatags, title and opengraph

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,9 @@ import Theme from '../styles/theme';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <head>
+        <title>Emanuel Suarez - React Developer </title>
+      </head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,16 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
+          
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <meta name="description" content="Desarrollador web con amplia experiencia en React y otras tecnologías modernas. Echa un vistazo a mi portfolio para ver algunos de mis trabajos y contáctame para discutir cómo puedo ayudarte a impulsar tu negocio en línea."/>
+          <meta name="keywords" content="developer, argentina, buenos aires, desarrollador web, react, portfolio, proyectos, "></meta>
+          {/* Open Graph */}
+          <meta property="og:title" content="Emanuel Suarez - Developer"/>
+          <meta property="og:type" content="Portfolio Website"/>
+          <meta property="og:image" content="URL de mi imagen"/>
+          <meta property="og:url" content="https://emanuelsuarez.com"/>
+          <meta property="og:description" content="Soy un desarrollador web altamente capacitado en React y otras tecnologías modernas. Echa un vistazo a mi portfolio para ver algunos de mis trabajos y contáctame para discutir cómo puedo ayudarte a impulsar tu negocio en línea."/>
         </Head>
         <body>
           <Main />
