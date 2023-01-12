896377ca5438b022da648f9da167318e56dd128f
ëmanuelsuarez¨ <braianesuarez@gmail.com>
2023-01-11 23:35:33 -0300

    [update] project buttons style

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -92,12 +92,12 @@ export const ExternalLinks = styled.a`
 color:#d4c0c0;
 font-size: 1.6rem;
 padding:1rem 1.5rem;
-background: #6b3030;
+background: ${({ alt }) => alt ? 'linear-gradient(270deg, #F46737 0%, #945DD6 100%)' : 'linear-gradient(270deg, #13ADC7 0%, #945DD6 100%)'};
 border-radius: 15px;
 transition: 0.5s;
 &:hover{
-  background: #801414;
-
+  background: ${({ alt }) => alt ? 'linear-gradient(70deg, #945DD6 0%, #F46737 100%)' : 'linear-gradient(70deg, #945DD6 0%, #13ADC7 100%)'};
+  
 }
 `;
 
